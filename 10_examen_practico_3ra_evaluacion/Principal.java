d55a5d3cc61b6d0985d7106314b80a086fdb4530
RetanaAlvarez <chepe_jm10@hotmail.com>
2022-05-17 07:48:37 -0500

    examen_practico_unidad 3

---
+++ 10_examen_practico_3ra_evaluacion/Principal.java
@@ -0,0 +1,22 @@
+public class Principal{
+	public static void main(String[] arg) {
+		Instances Prueba_Examen = pe;
+		Principal.Operacion_Prueba ((3,4,1)-->pe.System.out.println(a+(b+c)));
+		Principal.Operacion_Prueba((int)No1,(int)No2).System.out.println(No1+No2);
+		Practica_Examen.Mensaje_hola();
+		pe.Operacion_Prueba((int)a,(int)b,(int)c)-->System.out.println(a+(b*c));
+		pe.Operacion_Prueba(int a=10, int No, int No3);
+		Principal.Mi_metodo(pe);
+		Principal.Mi_metodo((int)a,(int)b,(int c)--> System.out.println(a+(b*c)));
+		Principal.pe(int)a,(int)b, (int)c)-->System.out.println(a+(b/c));
+		Principal.Mi_metodo(pe,a=7,b=7,c=8);
+	}
+	private static void Mi_metodo(Object Prueba_Examen){
+		Prueba_Examen.Operacion_Prueba(int a =10,int No1,int No3);
+	}
+	private staticvoid Mi_metodo(object Prueba_Examen,int a =6, int b=7,int c=7){
+		Prueba_Examen.Operacion_Prueba(a,Operacion_Prueba(int b,int c),Operacion_Prueba(int a, int b , int c)){
+		
+		}
+	}
+}
