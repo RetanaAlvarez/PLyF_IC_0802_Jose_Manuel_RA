9c3e75b8c0201dccefd2b0f680e7b8cf3efbc7db
RetanaAlvarez <chepe_jm10@hotmail.com>
2022-05-20 09:46:33 -0500

    examen escrito

---
+++ 10_examen_practico_3ra_evaluacion/Prueba_Examen.java
@@ -0,0 +1,15 @@
+Public Interfaz  Practica_examen{
+	public void Mensaje_hola(){
+		System.out.println("Hola mi nombre es Jose Manuel Retana Alvarez");
+	}
+	public void Mensaje_hola(String hola){
+		System.out.println("La cadena es "+hola);
+	}
+	public static Operacion_Prueba(int No1,int No2){
+		return(No1+No2);
+	}
+	public abstrac void Operacion_Prueba(int a, int b, int c){
+		
+	}
+
+}
